5f1d17d609c5dc1a67701b2f7ca43b1cbb804c85
Sooryamol-S <135025674+Sooryamol-S@users.noreply.github.com>
2024-10-07 22:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,51 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+	let ans='';
+	while (num > 0) {
+        if (num >= 1000) {
+            ans += 'M';
+            num -= 1000;
+        } else if (num >= 900) {
+            ans += 'CM';
+            num -= 900;
+        } else if (num >= 500) {
+            ans += 'D';
+            num -= 500;
+        } else if (num >= 400) {
+            ans += 'CD';
+            num -= 400;
+        } else if (num >= 100) {
+            ans += 'C';
+            num -= 100;
+        } else if (num >= 90) {
+            ans += 'XC';
+            num -= 90;
+        } else if (num >= 50) {
+            ans += 'L';
+            num -= 50;
+        } else if (num >= 40) {
+            ans += 'XL';
+            num -= 40;
+        } else if (num >= 10) {
+            ans += 'X';
+            num -= 10;
+        } else if (num >= 9) {
+            ans += 'IX';
+            num -= 9;
+        } else if (num >= 5) {
+            ans += 'V';
+            num -= 5;
+        } else if (num >= 4) {
+            ans += 'IV';
+            num -= 4;
+        } else {
+            ans += 'I';
+            num -= 1;
+        }
+    }
 
-  //your code here
+    return ans;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
